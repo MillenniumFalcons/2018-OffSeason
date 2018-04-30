2cd2428ea39262c139190f28df90fff1e65c9d94
Surya <suryamadhan@gmail.com>
2018-04-29 21:02:59 -0700

    make new Auton Idea folder and proj

---
+++ NewAutonIdea/src/org/usfirst/frc/team3647/robot/Robot.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3647.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends IterativeRobot 
+{
+
+	@Override
+	public void robotInit() 
+	{
+		
+	}
+
+	@Override
+	public void autonomousInit() 
+	{
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() 
+	{
+		
+	}
+
+	@Override
+	public void teleopPeriodic() 
+	{
+		
+	}
+
+	@Override
+	public void testPeriodic() 
+	{
+		
+	}
+}
+
