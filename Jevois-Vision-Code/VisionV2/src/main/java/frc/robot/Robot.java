d5f634009e6e99c7612534b81ab1924884a67645
Kunal Singla <tekunalogy@gmail.com>
2019-01-05 21:33:39 -0800

    wrote limelight class
    this has PID (centering to a target and following a target) for drivebase bot and can be easily implemented into other systems. To be used in 2019 code

--- Jevois-Vision-Code/VisionV2/src/main/java/frc/robot/Robot.java
+++ Jevois-Vision-Code/VisionV2/src/main/java/frc/robot/Robot.java
@@ -3,6 +3,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.*;
 import frc.team3647inputs.Joysticks;
+import frc.team3647inputs.Limelight;
+import frc.team3647inputs.NavX;
 import frc.team3647subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,87 +12,49 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class Robot extends IterativeRobot 
+public class Robot extends IterativeRobot
 {
 	Joysticks stick;
-	double rightSpeed;
-	double leftSpeed;
-	double speed;
-	double prevError = 0;
-	double sumError = 0;
-	NetworkTable table;
-	
-  
-  @Override
-  public void robotInit() 
-  {
-	  table = NetworkTableInstance.getDefault().getTable("limelight");
-	  stick = new Joysticks();
-	  Drivetrain.drivetrainInitialization(false);
-  }
+	Limelight one;
 
 
-
-  
-  	@Override
-  	public void autonomousInit() 
-  	{
-		Drivetrain.FRCarcadedrive(0.5, 0.5);
-  	}	
-
-  
-  	@Override
-  	public void autonomousPeriodic() 
-  	{
-	
-		NetworkTableEntry tx = table.getEntry("tx");
-		NetworkTableEntry ty = table.getEntry("ty");
-		NetworkTableEntry ta = table.getEntry("ta");
-		double x = tx.getDouble(0.0);
-		double y = ty.getDouble(0.0);
-		double area = ta.getDouble(0.0);
-
-		SmartDashboard.putNumber("LimelightX", x);
-		SmartDashboard.putNumber("LimelightY", y);
-		SmartDashboard.putNumber("LimelightArea", area);
-
-		System.out.println("X: " + x);
-		double s = .25;
-		// System.out.println("Y: " + y);
-		// System.out.println("Area: " + area);
-		if(x > 1 && x < -1)
-		{
-			Drivetrain.FRCarcadedrive(0, 0);
-
-		}
-		else if(x > 1)
-		{
-			Drivetrain.leftSRX.set(-s);
-			Drivetrain.rightSRX.set(s);
-		}
-		else
-		{
-			Drivetrain.leftSRX.set(s);
-			Drivetrain.rightSRX.set(-s);
-		}
-  	}
-	
-	  // This is the function that runs during Tele-Operated Period
-	public void teleopPeriodic() 
+	@Override
+	public void robotInit()
 	{
-		stick.setMainContollerValues();
-		// stick.setCoDriverContollerValues();
-		// System.out.println("It Has Begun");
-		// parseCam();
-		// visionPID(XCenter, YCenter);
-		Drivetrain.FRCarcadedrive(stick.rightJoyStickx, stick.leftJoySticky);
-		//visionPID(XCenter,YCenter);
+		one = new Limelight();
+		stick = new Joysticks();
+		Drivetrain.drivetrainInitialization(false);
 	}
 
-  
-  @Override
-  public void testPeriodic() 
-  {
+	int input = 1;
 
-  }
+	@Override
+	public void autonomousInit()
+	{
+		Drivetrain.FRCarcadedrive(0.5, 0.5);
+		Drivetrain.setToCoast();
+		input = 1;
+	}
+
+	
+	@Override
+	public void autonomousPeriodic()
+	{
+		one.follow(.45, .035, .9, .3, .01, 10); // Drivebase Bot -> kP = .45, kI = 0.035, kD = .9
+	}
+
+
+	// This is the function that runs during Tele-Operated Period
+	public void teleopPeriodic()
+	{
+		stick.setMainContollerValues();
+		Drivetrain.FRCarcadedrive(stick.rightJoyStickx, stick.leftJoySticky);
+
+	}
+
+	@Override
+	public void testPeriodic()
+	{
+
+	}
 }
